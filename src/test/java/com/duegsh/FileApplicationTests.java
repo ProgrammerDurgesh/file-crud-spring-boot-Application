846f82a1c76931d0316ef8427a753cacbf7bde93
Durgesh Yadav <durgeshyadavgpm16@gmail.com>
2023-08-19 10:08:13 +0530

    first commit

---
+++ src/test/java/com/duegsh/FileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duegsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
